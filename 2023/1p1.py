a27ad9cdbd9c0c44c4d210b4141b96164a659244
CatKasha <CatKasha@users.noreply.github.com>
2023-12-12 02:49:20 +0300

    2023 1p1

---
+++ 2023/1p1.py
@@ -0,0 +1,21 @@
+with open("1.txt", "r") as file:
+    lines = file.read().split("\n")
+    digits = []
+    for line in lines:
+        if (line == [""]):
+            continue
+
+        in_line_digits = []
+        for char in line:
+            if (char.isdigit()):
+                in_line_digits.append(char)
+
+        if (len(in_line_digits) == 1):
+            digits.append(in_line_digits[0] * 2)
+        else:
+            digits.append(in_line_digits[0] + in_line_digits[-1])
+
+    digits_sum = 0
+    for i in digits:
+        digits_sum += int(i)
+    print(digits_sum)
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # adventofcode
+my solutions to [Advent of Code](https://adventofcode.com/) puzzles
+
+after im done with the puzzle i will not edit my code and upload it in to repo as is (i may change some variable names, but not a logic of the program)
