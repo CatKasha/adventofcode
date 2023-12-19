8e2c28c74e6b6a0502dc10728f06854672c184e5
CatKasha <CatKasha@users.noreply.github.com>
2023-12-20 00:22:35 +0300

    2023 4p2

---
+++ 2023/4p2.py
@@ -0,0 +1,20 @@
+with open("4.txt", "r") as file:
+    lines = file.read().splitlines()
+
+    cards = [1] * len(lines)
+    for i, line in enumerate(lines):
+        win_num_s, nums = line.split(": ")[1].split(" | ")
+        win_num_s = win_num_s.split()
+        nums = nums.split()
+
+        matches = 0
+        for win_num in win_num_s:
+            if (win_num in nums):
+                matches += 1
+
+        if (matches != 0):
+            for j in range(matches):
+                for k in range(cards[i]):
+                    cards[i + 1 + j] += 1
+
+    print(sum(cards))
